ccaff8cd4c3ee19da50848d1f0331d39a6702cac
Gautam Patil <gautampatil4545@outlook.com>
2023-12-23 19:34:24 +0530

    Update settings.py

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
